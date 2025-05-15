27d4ebc777467663a248863bf907b7b287a9b2e6
Bosper-the-Magician <fabian.spinar@hallowelt.com>
2025-05-15 11:37:03 +0200

    add SQL-Patches

---
+++ root-fs/app/bin/upgrade5x/upgrade_db.sql
@@ -0,0 +1,21 @@
+CREATE TABLE /*_*/oathauth_types (
+  oat_id INT AUTO_INCREMENT NOT NULL,
+  oat_name VARBINARY(255) NOT NULL,
+  UNIQUE INDEX oat_name (oat_name),
+  PRIMARY KEY(oat_id)
+) /*$wgDBTableOptions*/;
+
+
+CREATE TABLE /*_*/oathauth_devices (
+  oad_id INT AUTO_INCREMENT NOT NULL,
+  oad_user INT NOT NULL,
+  oad_type INT NOT NULL,
+  oad_name VARBINARY(255) DEFAULT NULL,
+  oad_created BINARY(14) DEFAULT NULL,
+  oad_data BLOB DEFAULT NULL,
+  INDEX oad_user (oad_user),
+  PRIMARY KEY(oad_id)
+) /*$wgDBTableOptions*/;
+
+ALTER TABLE openid_connect DROP COLUMN oidc_id;
+ALTER TABLE openid_connect ADD PRIMARY KEY (oidc_user);
